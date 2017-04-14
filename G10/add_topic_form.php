af4a445ff181f651cfe1cb7c500b826eaa707102
Chang Chen <vlcpulane2@hotmail.com>
2017-04-13 19:09:34 -0700

    11

--- G10/add_topic_form.php
+++ G10/add_topic_form.php
@@ -37,9 +37,6 @@
 		</header>
 
 <div id="forumTable">
-<<<<<<< HEAD:G10/add_topic_form.php
-<table width="100%" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#CCCCCC">
-<tr>
 <form id="form1" name="form1" method="post" action="add_topic.php">
 <td>
 <table width="100%" border="0" cellpadding="3" cellspacing="1" bgcolor="#FFFFFF">
@@ -64,56 +61,6 @@
 </table>
 </td>
 </form>
-</tr>
-</table>
-=======
-			<?php
-			if (isLoggedIn()){
-				session_start();
-                echo $_SESSION['User_ID'];
-				echo '<a href="logout.php">Logout</a><br/>';
-			} else {
-				echo '<a href="login_form.php">Login</a><br/>';
-				echo '<a href="register_form.php">New user?</a>';
-			}
-			?>
-            <table>
-            <tr>
-            <td class="forumtd1"><strong>#</strong></td>
-            <td class="forumtd2"><strong>Topic</strong></td>
-            <td class="forumtd2"><strong>Date/Time</strong></td>
-            </tr>
-			<tr>
-            <td>1</td>
-            <td><a href="#">Coming Soon!</a><BR></td>
-            <td>16/02/17 03:31:04</td>
-            </tr>
-			<tr>
-            <td>2</td>
-            <td><a href="#">Coming Soon!</a><BR></td>
-            <td>16/02/17 03:31:04</td>
-            </tr>
-			<tr>
-            <td>3</td>
-            <td><a href="#">Coming Soon!</a><BR></td>
-            <td>16/02/17 03:31:04</td>
-            </tr>
-			<tr>
-            <td>4</td>
-            <td><a href="#">Coming Soon!</a><BR></td>
-            <td>16/02/17 03:31:04</td>
-            </tr>
-			<tr>
-            <td>5</td>
-            <td><a href="#">Coming Soon!</a><BR></td>
-            <td>16/02/17 03:31:04</td>
-            </tr>
-			<tr>
-			<td colspan="3" class="forumtd4"><a href="#"><strong>Create New Topic</strong> </a></td>
-			</td>
-            </tr>
-            </table>
->>>>>>> origin/master:G10/Forum.html
 </div>
 
 
