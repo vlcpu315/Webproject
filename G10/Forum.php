919dc535dc707e1b03c1076496dc64ceba62c29d
Chang Chen <vlcpulane2@hotmail.com>
2017-04-13 17:31:05 -0700

    1
    1

---
+++ G10/Forum.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>WonderCNF</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="css/forum.css">
+</head>
+	<body>
+	<div id="container">
+		<header>
+			<div class="banner">
+				<img src="image/Chihuo.Jpg" alt="Logo">WonderCNF
+			</div>
+			<!-- navigation -->
+<div class="navbar">
+				 <ul>
+					<li><a href="index.html" class="border_first">Home</a></li>
+					<li>
+						<a href="Fiveflavors.html">Five Flavors</a>
+							 <ul>
+								<li><a href="Sour.html">Sour</a></li>
+								<li><a href="Sweet.html">Sweet</a></li>
+								<li><a href="bitter.html">Bitter</a></li>
+								<li><a href="spicy.html">Spicy</a></li>
+								<li><a href="salty.html">Salty</a></li>
+							</ul>
+					</li>
+					<li><a href="Forum.html">Forum</a></li>
+					<li><a href="AboutUs.html">About Us</a></li>
+					<li><a href="Contact.html">Contact Us</a></li>
+				</ul>
+			</div>
+		</header>
+
+<div id="forumTable">
+			<div id = "login">
+				<?php
+				echo '<a href="login_form.php">Login</a><br/>';
+				echo '<a href="register_form.php">New user?</a>';
+				?>
+			</div>
+            <table>
+            <tr>
+            <td class="forumtd1"><strong>#</strong></td>
+            <td class="forumtd2"><strong>Topic</strong></td>
+            <td class="forumtd2"><strong>Date/Time</strong></td>
+            </tr>
+			<tr>
+            <td>1</td>
+            <td><a href="#">Coming Soon!</a><BR></td>
+            <td>16/02/17 03:31:04</td>
+            </tr>
+			<tr>
+            <td>2</td>
+            <td><a href="#">Coming Soon!</a><BR></td>
+            <td>16/02/17 03:31:04</td>
+            </tr>
+			<tr>
+            <td>3</td>
+            <td><a href="#">Coming Soon!</a><BR></td>
+            <td>16/02/17 03:31:04</td>
+            </tr>
+			<tr>
+            <td>4</td>
+            <td><a href="#">Coming Soon!</a><BR></td>
+            <td>16/02/17 03:31:04</td>
+            </tr>
+			<tr>
+            <td>5</td>
+            <td><a href="#">Coming Soon!</a><BR></td>
+            <td>16/02/17 03:31:04</td>
+            </tr>
+			<tr>
+			<td colspan="3" class="forumtd4"><a href="#"><strong>Create New Topic</strong> </a></td>
+			</td>
+            </tr>
+            </table>
+</div>
+
+
+
+<footer>
+			<P class="copyright"><a href="Sitemap.html">Site Map</a></P>
+			<br>
+			<p class="copyright">Copyright &copy; 2017 Group 10 | Designed by <a href="AboutUs.html">Victor</a>, <a href="AboutUs.html">Samuel</a>, <a href="AboutUs.html">Grace</a>, <a href="AboutUs.html">Bob</a></p>
+		</footer>
+</div>
+</body>
